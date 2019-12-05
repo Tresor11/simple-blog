37403c9b9f6d2606d65d9357846848886382ee92
Tresor11 <tresor.moise2001@gmail.com>
2019-12-05 13:33:38 +0200

    added the create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,23 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
-    @articles=Article.all
+    @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
   end
 end
