6c63835025fcfbd601a0a85eec78f0d10d8fff41
Tresor11 <tresor.moise2001@gmail.com>
2019-12-22 17:28:50 +0200

    added paperclip gem

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,6 +2,6 @@
 
 module CommentsHelper
   def comments_params
-    params.require(:comment).permit(:author_name, :body, :tag_list)
+    params.require(:comment).permit(:author_name, :body)
   end
 end
